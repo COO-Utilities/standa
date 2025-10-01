7b7e360b0de9d67b75f83e3541820eb97cf48628
Elijah AB <elijahab@caltech.edu>
2025-09-30 18:02:39 -0700

    Test dir and files added, mock test in the making.-Elijah.AB

---
+++ tests/default_smc8_tests.py
@@ -0,0 +1,10 @@
+#################
+#Outline Robust and Communication Tests
+#################
+
+import pytest
+pytestmark = pytest.mark.default
+import sys
+import os
+import unittest
+import time
---
+++ tests/physical_smc8_tests.py
@@ -0,0 +1,8 @@
+
+
+import pytest
+pytestmark = pytest.mark.integration
+import sys
+import os
+import unittest
+import time 
